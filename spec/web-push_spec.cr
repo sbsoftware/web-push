2e2280d3718924adf5f2b5a8008172416d12eac3
Stefan Bilharz <stefan@sbsoftware.de>
2026-02-13 17:48:28 +0100

    Crystal init

---
+++ spec/web-push_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Web::Push do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/web-push.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Web::Push`
+module Web::Push
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
